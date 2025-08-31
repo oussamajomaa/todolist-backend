4cded1819feb01c1d581e33f21ff01ba50788b9d
oussamajomaa <osm70@gmx.com>
2025-08-31 10:55:43 +0200

    samesite none

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -113,7 +113,7 @@ router.post('/login', async (req, res) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Strict',
+        sameSite: 'none',
         maxAge: 2 * 60 * 60 * 1000 // 2h
     })
 
